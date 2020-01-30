2d1411bf925dbd32e79d4802018814f66c2c978f
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-01-30 02:54:16 +0100

    appsody fix

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://auth_app:BatMan_2020@host.docker.internal:27017/mern-auth",
-    secretOrKey: "secret"
+  mongoURI: "mongodb://"+process.env.MONGODB_USER+":"+process.env.MONGODB_PASSWORD+"@"+process.env.MONOGDB_HOST+"/"+process.env.MONGODB_DATABASE,
+  secretOrKey: "secret"
   };
