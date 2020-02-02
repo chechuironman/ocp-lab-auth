6603a8aa0305e59d1536b72731a933afd679d1f9
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-02-02 13:17:27 +0100

    secret fix

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://"+process.env.MONGODB_USER+":"+process.env.MONGODB_PASSWORD+"@"+process.env.MONOGDB_HOST+"/"+process.env.MONGODB_DATABASE,
+  mongoURI: "mongodb://"+process.env.MONGODB_USER+":"+process.env.MONGODB_PASSWORD+"@"+process.env.MONGODB_HOST+"/"+process.env.MONGODB_DATABASE,
   secretOrKey: "secret"
   };
